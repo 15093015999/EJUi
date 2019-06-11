be216dfdce517d5655fbfd8f42d3097db9031f21
Lin825 <1275902560@qq.com>
2019-06-11 16:43:57 +0800

    王智林-顾客管理02

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,21 +1,62 @@
 import React from 'react';
-import { connect } from 'dva';
+// import { connect } from 'dva';
 import styles from './IndexPage.css';
+import { Menu } from 'antd';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+class IndexPage extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render(){
+    return(
+      <div className={styles.normal}>
+        <div className={styles.normal}>
+          <h1 className={styles.title}>易洁家政服务</h1>
+        </div>
+        {/* <div className={styles.welcome} />
+          <ul className={styles.list}></ul>
+        </div> */}
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+          <Menu.Item key="alipay">
+            <a href="http://localhost:8000/#/customer" target="_blank" rel="noopener noreferrer">
+              <h1 className={styles.title}>顾客管理</h1>
+            </a>
+          </Menu.Item>
+
+          {/* <Menu.Item key="alipay">
+            <a href="http://localhost:8000/#/course" target="_blank" rel="noopener noreferrer">
+              课程管理
+            </a>
+          </Menu.Item> */}
+
+          </Menu>
+      </div>
+    )
+  }
 }
 
-IndexPage.propTypes = {
-};
+// function IndexPage() {
+//   return (
+//     <div className={styles.normal}>
+//       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+//       <div className={styles.welcome} />
+//       <ul className={styles.list}>
+//         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+//         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default connect()(IndexPage);
+// IndexPage.propTypes = {
+// };
+
+export default (IndexPage);
