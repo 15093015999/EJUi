621fb4785a20e076daca67e90626ef8bc70d0f73
Kalearn <2441356412@qq.com>
2019-06-11 17:30:41 +0800

    拦截器qs配置

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
-import {message} from 'antd'
+import { message } from 'antd'
 import qs from 'qs'
 // 1. axios的默认配置
 axios.defaults.baseURL = "http://localhost:7890"
-axios.defaults.headers["Content-Type"]= "application/x-www-form-urlencoded";
+axios.defaults.headers["Content-Type"] = "application/x-www-form-urlencoded";
 
 
 // 2. 拦截器配置
-axios.interceptors.request.use((config)=>{
-  if(config.method === "post"){
-    config.data = qs.stringify(config.data)
+axios.interceptors.request.use((config) => {
+  if (config.method === "post") {
+    config.data = qs.stringify(config.data, { arrayFormat: 'repeat'});
   }
   return config;
 })
-axios.interceptors.response.use((response)=>{
-  let {data} = response;
+axios.interceptors.response.use((response) => {
+  let { data } = response;
   response.status = data.status;
   response.statusText = data.message;
   response.data = data.data;
   return response;
-},(error)=>{
+}, (error) => {
   message.error("服务端异常")
   return Promise.reject(error);
 })
