bde006a7d7a762972ce1509dc00df75e97979bfe
Lin825 <1275902560@qq.com>
2019-06-11 20:32:05 +0800

    王智林-客户管理03

--- src/routes/CustomerPage.js
+++ src/routes/CustomerPage.js
@@ -1,113 +1,141 @@
 import React from 'react';
 import styles from './CustomerPage.css';
-import { Button, Table, Icon, Popconfirm,message } from 'antd';
+import { Button, Table, Icon, Popconfirm, message, } from 'antd';
 import axios from '../utils/axios'
 
-// import { from } from '_array-flatten@2.1.2@array-flatten';
 
-class CustomerPage extends React.Component{
-    //局部状态state
-    constructor() {
-        super();
-        this.state = {
-            msg: "hello",
-            list: []
+class CustomerPage extends React.Component {
+  //局部状态state
+  constructor() {
+    super();
+    this.state = {
+      selectedRowKeys: [],
+      loading: false,
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    // 查询数据，进行数据绑定
+    this.handlerLoad();
+    // this.reloadDate();
+  }
+
+  //封装查询用户
+  handlerLoad() {
+    axios.get("/customer/findAll")
+      .then((result) => {
+        //console.log('查询到的数据为：',result.data);
+        //将查询到的数据设置到state中
+        this.setState({
+          list: result.data
+        })
+      })
+  }
+  //删除用户
+  handleDelete(id) {
+    let obj = { 'id': id }
+    axios.post("/customer/deleteById", obj)
+      .then((result) => {
+        if (200 === result.status) {
+          message.success(result.statusText)
+          this.handlerLoad();
+        } else {
+          message.error('删除失败，请稍后再试')
         }
-    }
+      })
+  }
+batchDelete=() => {
+    axios.post("/customer/batchDelete", {ids:this.state.selectedRowKeys})
+      .then((result) => {
+        if (200 === result.status) {
+          message.success(result.statusText)
+          this.handlerLoad();
+        } else {
+          message.error('删除失败，请稍后再试')
+        }
+      })
+}
+onSelectChange = selectedRowKeys => {
+  this.setState({ selectedRowKeys });
 
-    componentDidMount() {
-        // 查询数据，进行数据绑定
-        this.handlerLoad();
-    }
+};
 
-    // reloadDate(){
-    //     this.setState({loading:true});
-    //     axios.get("/customer/findAll")
-    //     .then((result)=>{
-    //         this.setState({list:result.data})
-    //     })
-    //     .finaly
-    // }
-    //封装查询用户
-    handlerLoad() {
-        axios.get("/customer/findAll")
-            .then((result) => {
-                //console.log('查询到的数据为：',result.data);
-                //将查询到的数据设置到state中
-                this.setState({
-                    list: result.data
-                })
-            })
-    }
-    //删除用户
-    handleDelete(id) {
-        let obj = {'id': id}
-        axios.post("/customer/deleteById", obj)
-            .then((result) => {
-                if (200 === result.status) {
-                    message.success(result.statusText)
-                    this.handlerLoad();
-                } else {
-                    message.error('删除失败，请稍后再试')
-                }
-            })
-    }
-
-    render(){
-        let text = "是否删除"
-        let columns = [{
-            title: "编号",
-            dataIndex: "id"
-        }, {
-            title: "姓名",
-            dataIndex: "realname"
-        }, {
-            title: "电话",
-            dataIndex: "telephone"
-        }, {
-            title: "密码",
-            dataIndex: "password"
-        }, {
-            title: "状态",
-            dataIndex: "status"
-        },{
-            title: "操作",
-            render: (Text, Record) => {
-                return (
-                    <div>
-                        <Popconfirm placement="top" title={text}
-                            onConfirm={this.handleDelete.bind(this, Record.id)} okText="是" cancelText="否">
-                            <Icon type="delete"></Icon>
-                        </Popconfirm>
-                        &nbsp;&nbsp;
+render() {
+  const { selectedRowKeys } = this.state;
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: this.onSelectChange,
+  };
+  let text = "是否删除"
+  let columns = [{
+    title: "编号",
+    dataIndex: "id"
+  }, {
+    title: "姓名",
+    dataIndex: "realname"
+  }, {
+    title: "电话",
+    dataIndex: "telephone"
+  }, {
+    title: "密码",
+    dataIndex: "password"
+  }, {
+    title: "状态",
+    dataIndex: "status"
+  }, {
+    title: "操作",
+    render: (table, Record) => {
+      return (
+        <div>
+          <Popconfirm placement="top" title={text}
+            onConfirm={this.handleDelete.bind(this, Record.id)} okText="是" cancelText="否">
+            <Icon type="delete"></Icon>
+          </Popconfirm>
+          &nbsp;&nbsp;
 
                     </div>
-                )
-            }
-        }]
-        return(
-            <div className="customer">
-                <div className={styles.customer}>
-                <div className={styles.title}>顾客管理</div>
-                </div>
-
-                <Button title="primary">添加</Button>&nbsp;
-                <Button>批量删除</Button>&nbsp;
-                <Button title="link" >退出</Button>
-
-                <Table 
-                    rowKey="id"
-                    size="small"
-                    // loading={true}
-                    columns={columns}
-                    dataSource={this.state.list}
-                />
- 
-            </div>
-
-
-        )
+      )
     }
+  }]
+
+  //返回结果
+  return (
+    <div className="customer">
+      <div className={styles.customer}>
+        <div className={styles.title}>顾客管理</div>
+      </div>
+
+      &nbsp;<Button title="primary">添加</Button>
+      &nbsp;
+        <Popconfirm
+        placement="bottomLeft"
+        title={text}
+        onConfirm={this.batchDelete}
+        okText="Yes"
+        cancelText="No"
+      >
+        <Button>批量删除</Button>
+      </Popconfirm>
+      &nbsp;<Button title="link" >退出</Button>
+
+      <Table
+        // bordered 
+        rowKey="id"
+        size="small"
+        bordered
+        // loading={this.state.loading}
+        // rowSelection={rowSelection}
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={this.state.list}
+      />
+
+    </div>
+
+
+  )
+}
 }
 
 export default CustomerPage;
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,62 +1,22 @@
 import React from 'react';
-// import { connect } from 'dva';
+import { connect } from 'dva';
 import styles from './IndexPage.css';
-import { Menu } from 'antd';
 
-class IndexPage extends React.Component {
-  state = {
-    current: 'mail',
-  };
-
-  handleClick = e => {
-    console.log('click ', e);
-    this.setState({
-      current: e.key,
-    });
-  };
-
-  render(){
-    return(
-      <div className={styles.normal}>
-        <div className={styles.normal}>
-          <h1 className={styles.title}>易洁家政服务</h1>
-        </div>
-        {/* <div className={styles.welcome} />
-          <ul className={styles.list}></ul>
-        </div> */}
-        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-          <Menu.Item key="alipay">
-            <a href="http://localhost:8000/#/customer" target="_blank" rel="noopener noreferrer">
-              <h1 className={styles.title}>顾客管理</h1>
-            </a>
-          </Menu.Item>
-
-          {/* <Menu.Item key="alipay">
-            <a href="http://localhost:8000/#/course" target="_blank" rel="noopener noreferrer">
-              课程管理
-            </a>
-          </Menu.Item> */}
-
-          </Menu>
-      </div>
-    )
-  }
+// 首页
+function IndexPage() {
+  return (
+    <div className={styles.normal}>
+      <h1 className={styles.title}>易洁家政服务</h1>
+      {/* <div className={styles.welcome} /> */}
+      <ul className={styles.list}>
+        {/* <li> <code></code> </li> */}
+        <li><a href="http://localhost:8000/#/customer">顾客管理</a></li>
+      </ul>
+    </div>
+  );
 }
 
-// function IndexPage() {
-//   return (
-//     <div className={styles.normal}>
-//       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-//       <div className={styles.welcome} />
-//       <ul className={styles.list}>
-//         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-//         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-//       </ul>
-//     </div>
-//   );
-// }
+IndexPage.propTypes = {
+};
 
-// IndexPage.propTypes = {
-// };
-
-export default (IndexPage);
+export default connect()(IndexPage);
