5fc7eb6e2640b9ac299059fe43e5429e71b32258
Kalearn <2441356412@qq.com>
2019-06-11 10:53:09 +0800

    axios

---
+++ src/utils/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {message} from 'antd'
+import qs from 'qs'
+// 1. axios的默认配置
+axios.defaults.baseURL = "http://localhost:7890"
+axios.defaults.headers["Content-Type"]= "application/x-www-form-urlencoded";
+
+
+// 2. 拦截器配置
+axios.interceptors.request.use((config)=>{
+  if(config.method === "post"){
+    config.data = qs.stringify(config.data)
+  }
+  return config;
+})
+axios.interceptors.response.use((response)=>{
+  let {data} = response;
+  response.status = data.status;
+  response.statusText = data.message;
+  response.data = data.data;
+  return response;
+},(error)=>{
+  message.error("服务端异常")
+  return Promise.reject(error);
+})
+
+export default axios;
