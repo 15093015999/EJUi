0ca1e63c7ff6742aa9dfa353dbe5739588f2e71d
Lin825 <1275902560@qq.com>
2019-06-11 16:20:13 +0800

    王智林-顾客管理01

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import CustomerPage from "./routes/CustomerPage";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/customer" exact component={CustomerPage} />
       </Switch>
     </Router>
   );
