f9a9f270d44d7e300702218f0646d815b07a3f15
Kalearn <2441356412@qq.com>
2019-06-12 10:30:28 +0800

    组长,主页面

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
-
+import { Link} from 'dva/router';
+import { Menu, Icon } from 'antd';
+const { SubMenu } = Menu;
 // 首页
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>易洁家政服务</h1>
-      {/* <div className={styles.welcome} /> */}
-      <ul className={styles.list}>
-        {/* <li> <code></code> </li> */}
-        <li><a href="#/customer">顾客管理</a></li>
-        <li><a href="#/category">分类管理</a></li>
-        <li><a href="#/order">订单管理</a></li>
-        <li><a href="#/product">商品管理</a></li>
-      </ul>
-    </div>
-  );
-}
+class IndexPage extends React.Component {
+  handleClick = e => {
+    console.log('click ', e);
+  };
 
-IndexPage.propTypes = {
-};
+  render() {
+
+    return (
+      <div className={styles.home}>
+        
+        <Menu
+          onClick={this.handleClick}
+          style={{ width: 256 }}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+        >
+        
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="appstore" />
+              <span>管理服务</span>
+            </span>
+          }
+        >
+          <Menu.Item key="1"><Link to="/customer">用户管理</Link></Menu.Item>
+          <Menu.Item key="2"><Link to="/category">分类管理</Link></Menu.Item>
+          <Menu.Item key="3"><Link to="/order">订单管理</Link></Menu.Item>
+          <Menu.Item key="4"><Link to="/product">商品管理</Link></Menu.Item>
+        </SubMenu>
+      </Menu>
+        <main className={styles.main}>
+        {this.props.children}
+        </main>
+        </div>
+    );
+  }
+
+}
 
 export default connect()(IndexPage);
