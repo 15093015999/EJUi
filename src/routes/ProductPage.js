81172074409d78e3d62698dbc39ec93d8b281fe5
hyy063 <870449479@qq.com>
2019-06-13 16:51:40 +0800

    改错

--- src/routes/ProductPage.js
+++ src/routes/ProductPage.js
@@ -16,7 +16,7 @@ class ProductPage extends React.Component {
       loading: true,
       list: [],
       visible: false,
-      product: {}
+
     }
   }
   UNSAFE_componentWillMount() {
@@ -30,6 +30,7 @@ class ProductPage extends React.Component {
 
   //封装查询
   handlerLoad() {
+    this.setState({loading:true})
     axios.get("/product/findAll")
       .then((result) => {
         //console.log('查询到的数据为：',result.data);
@@ -138,7 +139,7 @@ class ProductPage extends React.Component {
               <Button size="small"><Icon type="delete"></Icon></Button>
             </Popconfirm>
             &nbsp;&nbsp;
-          <Button size="small"><Icon type="edit" onClick={this.toEdit.bind(this, Record)}></Icon></Button>
+          <Button size="small" onClick={this.toEdit.bind(this, Record)}><Icon type="edit" ></Icon></Button>
           </div>
         )
       }
